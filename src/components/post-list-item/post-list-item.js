d8fd2e1314bfe1cf26e3f0132e60b524c6de801f
Olya <fedorovaolya0410@gmail.com>
2020-06-14 15:29:43 +0300

    little soc app done on react.js

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import './post-list-item.css';
 
-const PostListItem = ({label, important = false}) => {
 
-let classNames = 'app-list-item d-flex justify-content-between';
-if(important) {
+const PostListItem = ({label, onDelete, onToggleImportant, onToggleLiked, important, like}) => {
+ 
+    let classNames = 'app-list-item d-flex justify-content-between';
+
+    if(important) {
     classNames += ' important';
-}
+    }
 
-return (
-    <li className={classNames}>
-        <span className="app-list-item-label">
-                {label}
-            </span>
+    if(like) {
+      classNames += ' like';
+      }
+
+    return (
+      <div className={classNames}>
+        <span onClick={onToggleLiked} className="app-list-item-label">
+          {label}
+        </span>
         <div className="d-flex justify-content-center align-items-center">
-            <button
-              type="button" className="btn-star btn-sm">
-                <i className="fa fa-star"></i>
-            </button>
-            <button
-              type="button" className="btn-trash btn-sm">
-                <i className="fa fa-trash-o"></i>
-            </button>
-            <i className="fa fa-heart"></i>
+        <button
+          type="button"
+          className="btn-star btn-sm"
+          onClick={onToggleImportant}>
+          <i className="fa fa-star"></i>
+        </button>
+        <button
+          type="button"
+          className="btn-trash btn-sm"
+          onClick={onDelete}>
+            <i className="fa fa-trash-o"></i>
+        </button>
+        <i className="fa fa-heart"></i>
 
-            </div>
-        </li>
+        </div>
+    </div>
     )
+
 }
 
 export default PostListItem;
